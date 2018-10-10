a55ab299f80c676a85ddc065174ced81913792eb
kozzak2610 <kozzak2610@gmail.com>
2018-10-10 22:53:51 +0300

    first homework

---
+++ first/паліндром/script.js
@@ -0,0 +1,14 @@
+/*var text = prompt("Введіть слово на перевірку паліндрому ?");
+text.split('');
+
+var txet = text.reverse();
+alert(txet);*/
+var arr =  [];
+var text = prompt("Введіть слово на перевірку паліндрому ?");
+text.split('');
+for(let i = 0;i<=text.length;i++){
+    arr.push(text[i]);
+}
+arr.reverse();
+var str = arr.join('');
+alert(str);
